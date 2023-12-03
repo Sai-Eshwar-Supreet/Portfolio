b6067a820d94b9fe55d987d7bd993b99471dd052
Sai-Eshwar-Supreet <115769294+Sai-Eshwar-Supreet@users.noreply.github.com>
2023-12-03 16:29:00 +0530

    recent commit

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,7 +9,7 @@ export default function NavBar(){
             <div className='nav-bar'>
                 <Link className='logo' to="/Portfolio/">Sai Eshwar Supreet</Link>
                 <nav>
-                    <NavLink activeclassname="active" className="home-link" to="/Portfolio/" >
+                    <NavLink activeclassname="home-active" className="home-link" to="/Portfolio/" >
                         <FontAwesomeIcon icon={faHome} />
                     </NavLink>
                     <NavLink exact="true" activeclassname="active" className="about-link" to="/Portfolio/about" >
